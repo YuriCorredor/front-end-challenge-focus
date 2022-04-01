4f3251a6565c2887296da4fbda5a8b95fbc41e63
Yuri Corredor <YuriCorredor159@gmail.com>
2022-04-01 15:04:22 -0300

    Added opacity-50 to logo on hover with transition

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,9 @@ export default function Header() {
     return (
         <header className="bg-primary w-full flex justify-center px-2 sm:px-16 overflow-hidden">
             <div className="max-w-6xl w-full h-[125px] flex items-center justify-between">
-                <img className="justify-self-start h-auto w-auto max-w-[128px]" src="logo.png" />
+                <a target="_blank" href="https://focusnfe.com.br/">
+                    <img className="justify-self-start h-auto w-auto max-w-[128px] hover:opacity-50 transition-all cursor-pointer" src="logo.png" />
+                </a>
                 <h1 className="hidden sm:block mt-[30px] text-white font-semibold">Consulta de CEP (beta)</h1>
             </div>
         </header>
