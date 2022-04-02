ed0f91db4995cd86fb7e394e378ca73e51946482
Yuri Corredor <YuriCorredor159@gmail.com>
2022-04-02 13:33:24 -0300

    rel=noopener

--- components/main/Home.js
+++ components/main/Home.js
@@ -5,7 +5,7 @@ export default function Home() {
                 <h1 className="text-text_primary p-4 pt-0 text-3xl sm:text-5xl font-semibold">A API para emissão de notas e documentos fiscais, mas não somente</h1>
                 <p className="text-text_secondary text-md sm:text-lg font-medium p-4 py-0">A Focus fornece também "APIs acessórias" para facilitar a vida do desenvolvedor.</p>
                 <p className="text-text_secondary text-md sm:text-lg font-medium p-4 pt-0 pb-8">Conheça a nossa API de CEPs. Explore o quanto quiser!</p>
-                <a rel="noopener" target="_blank" href="https://focusnfe.com.br/doc/#consulta-de-cep-beta_consulta-de-cep-beta" className="mx-4 w-fit p-4 rounded-md text-[#F6F9FF] text-md font-medium bg-primary hover:scale-110 hover:shadow-lg transition-all cursor-pointer">Saiba Mais</a>
+                <a rel="noreferrer" target="_blank" href="https://focusnfe.com.br/doc/#consulta-de-cep-beta_consulta-de-cep-beta" className="mx-4 w-fit p-4 rounded-md text-[#F6F9FF] text-md font-medium bg-primary hover:scale-110 hover:shadow-lg transition-all cursor-pointer">Saiba Mais</a>
             </div>
             <img className="w-auto max-w-[300px] h-fit hidden lg:block" src='world.png' />
         </div>
