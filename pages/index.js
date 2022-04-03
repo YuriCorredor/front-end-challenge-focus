f436235749a7eb8dc5b89bb0ffd66b4696e4d334
Yuri Corredor <YuriCorredor159@gmail.com>
2022-04-03 15:41:43 -0300

    Added 'Next Head' to hold info as the Title, and some meta tags, as the description of the site.

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,16 @@ import Header from "../components/Header"
 import Main from "../components/Main"
 import Footer from "../components/Footer"
 import CallToAction from "../components/CallToAction"
+import Head from "next/head"
 
 export default function index() {
     return (
         <main>
+            <Head>
+            <title>Focus NFe</title>
+            <meta name="description" content="O Focus NFe é um conjunto de APIs de integração simples e rápida. Mantenha o seu foco, economize tempo e ganhe mais dinheiro integrando o seu sistema com a Focus."/>
+            <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+            </Head>
             <Header />
             <Main />
             <CallToAction />
